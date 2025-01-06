5e2b5a180a212e7d3dc10a7ce8aa385018d46f67
MichalDousa <146720370+MachalDouska@users.noreply.github.com>
2025-01-06 09:23:33 +0100

    Add files via upload

---
+++ coords/Coord.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace coords
+{
+    public class Coord : IEquatable<Coord>, IComparable<Coord>
+    {
+        public Coord(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+​
+        public int X { get; set; } 
+        public int Y { get; set; } 
+        public double Size { get { return Math.Sqrt(X * X + Y * Y); } }
+        public double Magnitude { get { return (X * X + Y * Y); } }
+
+        public static bool operator +(Coord x,  Coord y)
+        {
+
+        }
+
+        public bool Equals(Coord? other) {
+            if(other == null)
+        }
+
+        public int CompareTo(Coord? other) {
+            if (other.X == null || this.X == null || other.Y == null || this.Y == null)
+        }
+    }
+}
